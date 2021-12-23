af23c829c594bd28a3623311d81a2f6169627605
rashedul24 <ronyuap@gmail.com>
2021-12-23 10:52:53 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPaIWqwNpJo2zwDDfEO86bYQgGUctlSik",
+  authDomain: "burj-al-arab-5ee11.firebaseapp.com",
+  projectId: "burj-al-arab-5ee11",
+  storageBucket: "burj-al-arab-5ee11.appspot.com",
+  messagingSenderId: "924022696865",
+  appId: "1:924022696865:web:9e1a0df4cd4788ece9b9e0"
+};
+export default firebaseConfig;
